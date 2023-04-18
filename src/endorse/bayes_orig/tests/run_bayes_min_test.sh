7507b7890f326e6d21943f564b8f1dee6e15538b
Paulie14 <pavelexner@gmail.com>
2023-04-18 11:35:38 +0200

    Minor update to bayes min test.

--- src/endorse/bayes_orig/tests/run_bayes_min_test.sh
+++ src/endorse/bayes_orig/tests/run_bayes_min_test.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -x
 
-function safe_mkdir() {
-  DIR_PATH="$1"
-  if [ ! -d "$1" ]
-  then
-      mkdir -p "$1"
-  fi
-}
+TESTNAME=bayes_min_test
 
 # Setup paths
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -15,13 +9,13 @@ ENDORSE_SRC_ROOT="$SCRIPTPATH/../../../.."
 ENDORSE_SRC_BAYES="${SCRIPTPATH}"
 
 # Define config and workdir
-config=$SCRIPTPATH/config_bayes_min_test.yaml
-workdir=$ENDORSE_SRC_ROOT/tests/sandbox/bayes_min_test
+config=$SCRIPTPATH/config_$TESTNAME.yaml
+workdir=$ENDORSE_SRC_ROOT/tests/sandbox/$TESTNAME
 
 # copy config to workdir
 rm -r $workdir
-safe_mkdir $workdir
+mkdir -p $workdir
 cp "${config}" "${workdir}/config.yaml"
 
 # run simulation
-${ENDORSE_SRC_ROOT}/bin/endorse-bayes -t run -n 19 -o $workdir
+${ENDORSE_SRC_ROOT}/bin/endorse-bayes -t run -n 2 -o $workdir
