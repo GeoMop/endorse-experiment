da697e65a0a853e94e6139f1d2f6fdc42fb92705
Paulie14 <pavelexner@gmail.com>
2023-07-13 17:23:52 +0200

    Update running set of selected Flow123d samples.

--- src/endorse/bayes_orig/run_set_flow123d.py
+++ src/endorse/bayes_orig/run_set_flow123d.py
@@ -11,7 +11,8 @@ from measured_data import MeasuredData
 from run_all import setup
 from preprocess import preprocess
 
-from surrDAMH.modules import visualization_and_analysis as va
+from surrDAMH.modules.raw_data import RawData
+from surrDAMH.modules.analysis import Analysis
 
 
 def just_run_flow123d(measured_data, params_in, output_dir_in, boreholes_in):
@@ -34,42 +35,30 @@ def just_run_flow123d(measured_data, params_in, output_dir_in, boreholes_in):
 
 
 def get_best_accepted_params(config_dict_in, output_dir_in, count):
-
-    data_dir = os.path.join(output_dir_in, "saved_samples", "config_mcmc_bayes")
     no_parameters = len(config_dict_in['surrDAMH_parameters']['parameters'])
-
-    samples = va.Samples()
-    samples.load_MH(data_dir, no_parameters)
-    samples.calculate_properties()
-    samples.load_MH_with_posterior(data_dir, no_parameters)
-
-    # print(samples.x)
-    # print(samples.x_compress)
-    # print(samples.posteriors)
-
-    # modus = samples.find_modus()
-    # print("MODUS:\n", modus)
-
-    modi = samples.find_n_modi(count)
-    print(count, " best MODI:\n", modi)
 
     conf_bayes_path = os.path.join(output_dir_in, "common_files", config_dict_in["surrDAMH_parameters"]["config_file"])
     with open(conf_bayes_path) as f:
         config_bayes = yaml.safe_load(f)
     observations = np.array(config_bayes["problem_parameters"]["observations"])
-    best_fit = samples.find_best_fit(os.path.join(data_dir, "raw_data"), no_parameters, observations)
-    print("best fit:\n", best_fit[0])
-    best_fit_params = best_fit[0]
+    basename = os.path.basename(conf_bayes_path)
+    problem_name, fext = os.path.splitext(basename)
+    output_dir_bayes = os.path.join(output_dir_in, 'saved_samples', problem_name)
+
+    raw_data = RawData()
+    raw_data.load(output_dir_bayes, no_parameters, len(observations))
+    # type: 0-accepted, 1-prerejected, 2-rejected
+    raw_data_filtered = raw_data.filter(types=[0, 2], stages=[0])
+
+    analysis_pe = Analysis(config=config_bayes, raw_data=raw_data_filtered)
+    fits, norms = analysis_pe.find_n_best_fits(observations, count=count, norm="L2")
 
     param_file = os.path.join(output_dir_in, "best_accepted_params.csv")
     params = []
     with open(param_file, 'w') as file:
-        params.append(best_fit_params)
-        line = ','.join([str(s) for s in best_fit_params])
-        file.write(line + "\n")
-        for post, i, li, mod in modi:
-            params.append(mod)
-            line = ','.join([str(s) for s in mod])
+        for sample in fits:
+            params.append(sample.parameters())
+            line = ','.join([str(s) for s in sample.parameters()])
             file.write(line + "\n")
 
     return params
@@ -77,7 +66,6 @@ def get_best_accepted_params(config_dict_in, output_dir_in, count):
 
 if __name__ == "__main__":
 
-    # RUN THE MCMC SIMULATION
     # default parameters
     output_dir = None
     csv_data = None
@@ -104,7 +92,7 @@ if __name__ == "__main__":
     md.initialize()
 
     boreholes = config_dict["surrDAMH_parameters"]["observe_points"]
-    times, values = md.generate_measured_samples(boreholes)
+    times, values = md.generate_measured_samples(boreholes, [])
 
     if csv_data:
         print("Reading parameters from CSV: ", csv_data)
