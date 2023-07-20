e1f878752b8f5286749e2ba6a82917c3ad9254ab
Paulie14 <pavelexner@gmail.com>
2023-07-20 17:22:12 +0200

    Prepare sensitivity analysis, collect differences.

--- src/endorse/bayes_orig/run_sensitivity_analysis.py
+++ src/endorse/bayes_orig/run_sensitivity_analysis.py
@@ -31,7 +31,7 @@ def read_sensitivity_config(work_dir):
     sens_config_dict["rep_dir"] = os.path.abspath(os.path.join(sens_config_dict["script_dir"], "../../.."))
     return sens_config_dict
 
-def prepare_sets_of_params(config_dict_in, output_dir_in, n_processes, count):
+def prepare_sets_of_params(config_dict_in, output_dir_in, n_processes, n_best_fits):
     no_parameters = len(config_dict_in['surrDAMH_parameters']['parameters'])
 
     conf_bayes_path = os.path.join(output_dir_in, "common_files", config_dict_in["surrDAMH_parameters"]["config_file"])
@@ -42,16 +42,22 @@ def prepare_sets_of_params(config_dict_in, output_dir_in, n_processes, count):
     problem_name, fext = os.path.splitext(basename)
     output_dir_bayes = os.path.join(output_dir_in, 'saved_samples', problem_name)
 
+    # read posterior estimates for std
+    with open(os.path.join(output_dir_bayes, "output.yaml")) as f:
+        output_yaml = yaml.safe_load(f)
+        estimates = output_yaml["estimated_distributions"]
+        estimates_std = np.array([float(s["sigma"]) for s in estimates])
+
     raw_data = RawData()
     raw_data.load(output_dir_bayes, no_parameters, len(observations))
     # type: 0-accepted, 1-prerejected, 2-rejected
     raw_data_filtered = raw_data.filter(types=[0, 2], stages=[0])
 
     analysis_pe = Analysis(config=config_bayes, raw_data=raw_data_filtered)
-    fits, norms = analysis_pe.find_n_best_fits(observations, count=count, norm="L2")
+    fits, norms = analysis_pe.find_n_best_fits(observations, count=n_best_fits, norm="L2")
 
-    bin = int(count/n_processes)
-    rest_bin = count - n_processes*bin
+    bin = int(n_best_fits/n_processes)
+    rest_bin = n_best_fits - n_processes*bin
     bins = []
     for i in range(n_processes):
         bins.append(bin)
@@ -61,7 +67,11 @@ def prepare_sets_of_params(config_dict_in, output_dir_in, n_processes, count):
 
     sample_indices = np.array([s.idx for s in fits])
     offset = 0
-    print(bins)
+    # close diff: 0.05 sigma of posterior
+    # distant diff: 0.15 sigma of posterior
+    diff = 0.05*np.diag(estimates_std)
+    # print(diff)
+    # print(bins)
     for n, b in enumerate(bins):
         parameters = raw_data.parameters[sample_indices[offset:offset+b], :]
         param_file = os.path.join(output_dir_in, "sensitivity", "params_" + str(n).zfill(2) + ".csv")
@@ -70,10 +80,18 @@ def prepare_sets_of_params(config_dict_in, output_dir_in, n_processes, count):
             line = ','.join(analysis_pe.par_names)
             file.write(line + "\n")
 
-            print(np.shape(parameters))
-            for i in range(np.shape(parameters)[0]):
-                line = ','.join([str(s) for s in parameters[i,:]])
-                file.write(line + "\n")
+            # print(np.shape(parameters))
+            shape = np.shape(parameters)
+            for i in range(shape[0]):
+                n_diffs = 4*no_parameters+1
+                par_matrix = np.tile(parameters[i,:], (n_diffs,1))
+                diffs = np.vstack((0 * parameters[i, :],    # orig parameters
+                                   diff, -diff,             # close forward, backward diff
+                                   3*diff, -3*diff))        # distant forward, backward diff
+                par_matrix = par_matrix + diffs
+                for k in range(n_diffs):
+                    line = ','.join([str(s) for s in par_matrix[k,:]])
+                    file.write(line + "\n")
 
         offset = offset + b
 
@@ -116,11 +134,13 @@ def prepare_pbs_scripts(sens_config_dict, output_dir, np):
         lines = [
             *common_lines,
             '\n# finally gather the full command',
-            '\n' + os.path.join(endorse_root, "bin", "endorse-bayes") + " "
+            os.path.join(endorse_root, "bin", "endorse-bayes") + " "
                 + ' '.join(["-t", "set", "-o", output_dir, "-p", csv_file, "-x", sample_subdir, "-s", id]),
             # 'zip -r samples.zip solver_*', # avoid 'bash: Argument list too long'
             # 'find . -name "solver_*" -print0 | xargs -0 tar -zcvf samples.tar.gz',
             # 'find . -name "solver_*" -print0 | xargs -0 rm -r',
+            '\n' + ' '.join(['tar', '-zcvf', 'samples_' + id + '.tar.gz', sample_subdir]),
+            ' '.join(['rm', '-r', sample_subdir]),
             'echo "FINISHED"'
         ]
         pbs_file = os.path.join(output_dir, "sensitivity", "pbs_job_" + id + ".sh")
@@ -158,20 +178,9 @@ if __name__ == "__main__":
     sensitivity_dir = os.path.join(output_dir, "sensitivity")
     aux_functions.force_mkdir(sensitivity_dir, force=True)
 
+    # plan sample parameters a prepare them in CSV
     prepare_sets_of_params(config_dict, output_dir, n_processes, n_best_params)
 
+    # plan parallel sampling, prepare PBS jobs
     pbs_file_list = prepare_pbs_scripts(sens_config_dict, output_dir, n_processes)
 
-    # for pbs in pbs_file_list:
-    #     os.system("qsub " + pbs)
-
-    # if csv_data:
-    #     print("Reading parameters from CSV: ", csv_data)
-    #     with open(csv_data, newline='') as csvfile:
-    #         parameters = list(csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC))
-    # else:
-    #     print("Getting " + str(n_best_params) + " best parameters.")
-    #     parameters = get_best_accepted_params(config_dict, output_dir, n_best_params)
-
-    # JUST RUN FLOW123D FOR TESTING
-    # just_run_flow123d(config_dict, md, parameters, output_dir)
