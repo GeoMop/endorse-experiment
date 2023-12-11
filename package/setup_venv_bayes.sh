3b4ccac5da086f0b89319521a6991a428da850ec
Paulie14 <pavelexner@gmail.com>
2023-12-11 10:02:51 +0100

    Improve setup venv.

--- package/setup_venv_bayes.sh
+++ package/setup_venv_bayes.sh
@@ -6,31 +6,18 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 ENDORSE_SRC_ROOT="$SCRIPTPATH/.."
 cd "$ENDORSE_SRC_ROOT"
 
+rm -r venv_bayes
 python3 -m venv --system-site-packages venv_bayes
 
-source venv_bayes/bin/activate
-python3 --version
-which python
-which pip
+venv_pip=${ENDORSE_SRC_ROOT}/venv_bayes/bin/pip
 
-pip3 install wheel
-pip3 install --upgrade pip
-python3 -m pip install -r requirements.txt
-pip -V
+${ENDORSE_SRC_ROOT}/venv_bayes/bin/python3 --version
 
-# already installed in image: numpy
-pip install pandas statsmodels
-# ruamel keeps comments and order when loading and dumping yaml around
-pip install ruamel.yaml
+$venv_pip install -r requirements.txt
 
-# if not included in docker image
-# pip install mpi4py
+$venv_pip install -e submodules/bgem
+$venv_pip install --force-reinstall --upgrade attrs
 
-pip install -e submodules/bgem
-pip install -e submodules/surrDAMH
-pip install -e submodules/redis-cache
-pip install -e . # install endorse
-
-#pip freeze
-deactivate
-
+$venv_pip install -e submodules/surrDAMH
+#$venv_pip install -e submodules/redis-cache
+$venv_pip install -e . # install endorse
