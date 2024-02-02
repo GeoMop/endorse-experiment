a5a3ac92baad3a307be449eb7bbb1462bf975c96
Jan Brezina <jan.brezina@tul.cz>
2024-02-02 17:19:21 +0100

    minor main script changes

--- tests/Bukov2/borehole_data.py
+++ tests/Bukov2/borehole_data.py
@@ -1,4 +1,10 @@
 """
+Usage:
+
+python borehole_data.py workdir [from_sample]
+
+TODO: this use lot of memory and must be executed on a single allocated node, make this process part of the script similar to the process_boreholes.py
+
 Prepare sampled pressure data on individual boreholes.
 Write to separate files in order to paralelize the chamber optimization.
 Slice paralelization of this process is possible but it is bound by the IO of
@@ -9,9 +15,10 @@ allowing better overleap of calculation and communication and also keeping files
 """
 import sys
 import logging
+import subprocess
 
-import h5py
-import numpy as np
+#import h5py
+#import numpy as np
 from pathlib import Path
 from endorse.Bukov2.sample_storage import dataset_name, failed_ids_name, done_ids_name
 from endorse.common import load_config
@@ -19,6 +26,8 @@ from endorse.Bukov2 import sa_problem, boreholes
 from endorse.Bukov2.bukov_common import memoize, file_result, load_cfg
 params_name="parameters"
 from pathlib import Path
+script_path = Path(__file__).absolute()
+
 
 def main(workdir, from_sample):
     cfg_file = workdir / "Bukov2_mesh.yaml"
@@ -27,10 +36,58 @@ def main(workdir, from_sample):
     bh_field = boreholes.project_field(workdir, cfg, bh_set, from_sample=from_sample)
     print("Updated: ", bh_field.data_files)
 
+
+pbs_script_template = """
+#!/bin/bash
+#PBS -j oe
+#PBS -m e
+set -x 
+env | grep PBS_
+umask 0007
+echo "===="
+{python} {script_path} {workdir} {args}
+"""
+def submit_pbs(workdir, args):
+    workdir, cfg = load_cfg(workdir / "Bukov2_mesh.yaml")    
+    queue = cfg.pbs.queue
+    pbs_filename = workdir / "borehole_data.pbs"
+    parameters = dict(
+        python=sys.executable,
+        script_path=script_path,
+        workdir=workdir,
+        args=" ".join(args)
+    )
+    print(parameters['python'])
+    pbs_script = pbs_script_template.format(**parameters)
+    with open(pbs_filename, "w") as f:
+        f.write(pbs_script)
+    
+    
+    cmd = ['qsub', '-q', queue, '-l', f'select=1:ncpus=1:mem=90gb', pbs_filename]
+    subprocess.run(cmd, check=True)
+
+
+# TODO: use hyperque:
+# - can interact from python
+# - can wait for completed tasks
+# - simple task workflow with explicit task dependency is clearly possible
+# Could be a topic for a BP
+
+
+
+
 if __name__ == '__main__':
     workdir = Path(sys.argv[1]).absolute()
-    if len(sys.argv) > 2:
-        from_sample = int(sys.argv[2])
+    print(sys.argv)
+    if len(sys.argv) > 2 and (sys.argv[2] == 'submit'):
+        # submit the job
+        print('submit')
+        submit_pbs(workdir, sys.argv[3:])
     else:
-        from_sample = 0
-    main(workdir, from_sample)
+        if len(sys.argv) > 2:
+            from_sample = int(sys.argv[2])
+        else:
+            from_sample = 0
+
+        # run the borhehole data preparation.
+        main(workdir, from_sample)
