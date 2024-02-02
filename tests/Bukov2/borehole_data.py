fc41a18e75c031d092975f90dec5589a1a1500cd
Jan Brezina <jan.brezina@tul.cz>
2024-02-02 17:31:41 +0100

    qsub in borehole_data.py

--- tests/Bukov2/borehole_data.py
+++ tests/Bukov2/borehole_data.py
@@ -1,4 +1,10 @@
 """
+Usage:
+
+python borehole_data.py workdir [from_sample]
+
+TODO: this use lot of memory and must be executed on a single allocated node, make this process part of the script similar to the process_boreholes.py
+
 Prepare sampled pressure data on individual boreholes.
 Write to separate files in order to paralelize the chamber optimization.
 Slice paralelization of this process is possible but it is bound by the IO of
@@ -27,10 +33,56 @@ def main(workdir, from_sample):
     bh_field = boreholes.project_field(workdir, cfg, bh_set, from_sample=from_sample)
     print("Updated: ", bh_field.data_files)
 
+
+pbs_script_template = """
+#!/bin/bash
+#PBS -j oe
+#PBS -m e
+set -x 
+env | grep PBS_
+umask 0007
+echo "===="
+{python} {script_path} {workdir} {args}
+"""
+def submit_pbs(workdir, args):
+    workdir, cfg = bcommon.load_cfg(workdir / "Bukov2_mesh.yaml")    
+    queue = cfg.pbs.queue
+    pbs_filename = workdir / "borehole_data.pbs"
+    parameters = dict(
+        python=sys.executable,
+        script_path=script_path,
+        workdir=workdir,
+        args=" ".join(args)
+    )
+    print(parameters['python'])
+    pbs_script = pbs_script_template.format(**parameters)
+    with open(pbs_filename, "w") as f:
+        f.write(pbs_script)
+    
+    
+    cmd = ['qsub', '-q', queue, '-l', f'select=1:ncpus=1:mem=90gb', pbs_filename]
+    subprocess.run(cmd, check=True)
+
+
+# TODO: use hyperque:
+# - can interact from python
+# - can wait for completed tasks
+# - simple task workflow with explicit task dependency is clearly possible
+# Could be a topic for a BP
+
+
+
+
 if __name__ == '__main__':
     workdir = Path(sys.argv[1]).absolute()
+    if len(sys.argv) > 2 and (sys.argv[2] == 'submit'):
+        # submit the job
+        submit_pbs(workdir, argv[3:])
+
     if len(sys.argv) > 2:
         from_sample = int(sys.argv[2])
     else:
         from_sample = 0
+
+    # run the borhehole data preparation.
     main(workdir, from_sample)
