73c374849b3d547694f222d5b6a1344c75747517
Radek Srb <radeksrb@email.cz>
2024-05-03 00:07:05 +0200

    pickle_remove_duplicit

--- src/endorse/common/memoize2.py
+++ src/endorse/common/memoize2.py
@@ -6,6 +6,8 @@ import pickle
 import functools
 import hashlib
 import cloudpickle
+import io
+import pickletools
 
 
 _config = {
@@ -62,7 +64,7 @@ def memoize(cache):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             m = hashlib.sha256()
-            m.update(cloudpickle.dumps((func, args, kwargs)))
+            m.update(pickle_remove_duplicit(cloudpickle.dumps((func, args, kwargs))))
             hash = m.digest()
 
             fun_name = func.__name__
@@ -89,3 +91,85 @@ def memoize(cache):
 
         return wrapper
     return decorator
+
+
+def pickle_remove_duplicit(p):
+    'Optimize a pickle string by removing unused PUT opcodes and duplicated unicode strings'
+    put = 'PUT'
+    get = 'GET'
+    oldids = set()          # set of all PUT ids
+    newids = {}             # set of ids used by a GET opcode
+    opcodes = []            # (op, idx) or (pos, end_pos)
+    proto = 0
+    protoheader = b''
+    strings = {}
+    memo_map = {}
+    last_opcode_name = ""
+    last_arg = None
+    for opcode, arg, pos, end_pos in pickletools._genops(p, yield_end_pos=True):
+        if 'PUT' in opcode.name:
+            oldids.add(arg)
+            opcodes.append((put, arg))
+        elif opcode.name == 'MEMOIZE':
+            idx = len(oldids)
+            if 'BINUNICODE' in last_opcode_name:
+                if last_arg in strings:
+                    opcodes.pop()
+                    strid = strings[last_arg]
+                    newids[strid] = None
+                    opcodes.append((get, strid))
+                    memo_map[idx] = strid
+                else:
+                    strings[last_arg] = idx
+            oldids.add(idx)
+            opcodes.append((put, idx))
+        elif 'FRAME' in opcode.name:
+            pass
+        elif 'GET' in opcode.name:
+            if opcode.proto > proto:
+                proto = opcode.proto
+            if arg in memo_map:
+                arg = memo_map[arg]
+            newids[arg] = None
+            opcodes.append((get, arg))
+        elif opcode.name == 'PROTO':
+            if arg > proto:
+                proto = arg
+            if pos == 0:
+                protoheader = p[pos:end_pos]
+            else:
+                opcodes.append((pos, end_pos))
+        else:
+            opcodes.append((pos, end_pos))
+        last_opcode_name = opcode.name
+        last_arg = arg
+    del oldids
+
+    # Copy the opcodes except for PUTS without a corresponding GET
+    out = io.BytesIO()
+    # Write the PROTO header before any framing
+    out.write(protoheader)
+    pickler = pickle._Pickler(out, proto)
+    if proto >= 4:
+        pickler.framer.start_framing()
+    idx = 0
+    for op, arg in opcodes:
+        frameless = False
+        if op is put:
+            if arg not in newids:
+                continue
+            data = pickler.put(idx)
+            newids[arg] = idx
+            idx += 1
+        elif op is get:
+            data = pickler.get(newids[arg])
+        else:
+            data = p[op:arg]
+            frameless = len(data) > pickler.framer._FRAME_SIZE_TARGET
+        pickler.framer.commit_frame(force=frameless)
+        if frameless:
+            pickler.framer.file_write(data)
+        else:
+            pickler.write(data)
+    pickler.framer.end_framing()
+    return out.getvalue()
