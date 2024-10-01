3760e56105e47a45bc2bb15c95f4b2ff0aa0e39d
jstebel <jan.stebel@tul.cz>
2024-10-01 13:09:07 +0200

    Update plotting of selected boreholes and chambers.

--- src/endorse/Bukov2/optimize_configs.py
+++ src/endorse/Bukov2/optimize_configs.py
@@ -268,7 +268,7 @@ def export_vtk_bh_chamber_set(cfg_file, bh_pk_ids, fname, plot=False):
     all_packer_coords, bounds = bh_set.points(cfg)
 
     def t_for_idx(bi, idx):
-        return (idx - bh_set.boreholes[bi].start[0]) / bh_set.boreholes[bi].unit_direction[0]
+        return -(idx - bh_set.boreholes[bi].start[0]) / bh_set.boreholes[bi].unit_direction[0] # for zk40 there must be "-" before the expression, for zk30_32 "+"
     def single_bh_packers(bi, bid, pids):
         packers = [
             t_for_idx(bi, all_packer_coords[bi, pi, 0])
@@ -284,8 +284,8 @@ def export_vtk_bh_chamber_set(cfg_file, bh_pk_ids, fname, plot=False):
     for (bi,(bid,pids)) in zip(bh_indices,bh_pk_ids):
         bh_workdir = process_bh.borehole_dir(workdir, bi)
         index, data = process_bh._chamber_sensitivities(bh_workdir, cfg, chambers=None)
-        i_begin = [pids[i] for i in range(len(pids)-1)]
-        i_end = [pids[i+1]-2 for i in range(len(pids)-1)]
+        i_begin = [pids[i]-2 for i in range(len(pids)-1)]
+        i_end = [pids[i]+2 for i in range(len(pids)-1)]
         bh_sens = [data[index[i_begin, i_end],:] for i_begin, i_end in zip(i_begin, i_end)]
         bh_sens.insert(0, data[-1, :])
         sensitivities.append(bh_sens)
--- tests/Bukov2/test_config_plot.py
+++ tests/Bukov2/test_config_plot.py
@@ -2,18 +2,22 @@ from endorse.Bukov2 import optimize_configs as oc
 from pathlib import Path
 script_dir = Path(__file__).absolute().parent
 
-
+# borehole ID must be defined in subset in the Bukov2_mesh.yaml file
+# indices * 0.5 m = offset of chamber centers from borehole head
 configs = \
 [[
     #('P +5-25+15', [3,9,14,24]),
     #('P-13+66+12', [6, 13,19,26]),  # OK
     #('P-13+59-12', [16,21,28,34]),
-    ('P-13+70-15', [4,10,16,26]),
+    # ('P-13+70-15', [0,6,12,18,24,30,36,42,48,54,60,66]), #('P-13+70-15', [4,10,16,26]),
+#    ('P-13+70-15', range(2, 70, 2)),
+    ('L-11+67-17', [4,9,14]),
+    ('L -9+40+14', [5,12,18]),
+
     #('P -6+13-07', [11,18,24,30]),
-    #('P +5-09+00', [18, 26, 32, 38])
+#    ('P +5-09+00', [18, 26, 32, 38])
 ],
 ]
-
-cfg_file = script_dir / "3d_model/PE_01_02/Bukov2_mesh.yaml"
+cfg_file = script_dir / "PE_01_02_zk40/Bukov2_mesh.yaml"
 for i, cfg in enumerate(configs):
     oc.export_vtk_bh_chamber_set(cfg_file, cfg, f"boreholes_opt_cfg_{i}.vtk", plot=False)
